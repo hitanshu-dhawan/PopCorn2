1bae5c19fd5ff530d323a1f5661078ea855b1eea
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-10-28 15:25:10 +0530

    Initial commit

---
+++ app/src/main/java/com/hitanshudhawan/popcorn2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hitanshudhawan.popcorn2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
