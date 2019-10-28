7e30dc5736a03ac13715b84e4463a68813ae2260
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-10-28 16:21:22 +0530

    added MoviesFragment

---
+++ app/src/main/java/com/hitanshudhawan/popcorn2/MoviesFragment.kt
@@ -0,0 +1,16 @@
+package com.hitanshudhawan.popcorn2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MoviesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_movies, container, false)
+        return view
+    }
+
+}
