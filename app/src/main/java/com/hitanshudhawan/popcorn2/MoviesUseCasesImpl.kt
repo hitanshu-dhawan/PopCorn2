98030db7c5d3bca8c2b0ac437255c19e4ccb1071
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-12-18 04:27:01 +0530

    handled null

--- app/src/main/java/com/hitanshudhawan/popcorn2/MoviesUseCasesImpl.kt
+++ app/src/main/java/com/hitanshudhawan/popcorn2/MoviesUseCasesImpl.kt
@@ -26,7 +26,7 @@ class MoviesUseCasesImpl(
 
     private fun getShowBannerData(movieBriefs: Resource<List<MovieBrief>>, genres: Resource<List<Genre>>): Resource<List<ShowBannerData>> {
         if (movieBriefs is Resource.Success && genres is Resource.Success)
-            return Resource.Success(movieBriefs.data.map { ShowBannerData(it.backdrop!!, it.title, it.rating, it.genreIds.map { id -> genres.data.find { id == it.id }!!.name }) })
+            return Resource.Success(movieBriefs.data.map { ShowBannerData(it.backdrop ?: "", it.title, it.rating, it.genreIds.map { id -> genres.data.find { id == it.id }!!.name }) })
         if (movieBriefs is Resource.Error || genres is Resource.Error)
             return Resource.Error()
         return Resource.Loading()
@@ -34,7 +34,7 @@ class MoviesUseCasesImpl(
 
     private fun getShowCardData(movieBriefs: Resource<List<MovieBrief>>): Resource<List<ShowCardData>> {
         if (movieBriefs is Resource.Success)
-            return Resource.Success(movieBriefs.data.map { ShowCardData(it.poster!!, it.title) })
+            return Resource.Success(movieBriefs.data.map { ShowCardData(it.poster ?: "", it.title) })
         if (movieBriefs is Resource.Error)
             return Resource.Error()
         return Resource.Loading()
