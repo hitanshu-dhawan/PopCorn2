5d073997d81772d72fd1faa885fa81df84a36bf6
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-12-21 00:31:20 +0530

    implemented caching for GenresRepository

--- app/src/main/java/com/hitanshudhawan/popcorn2/GenresRepositoryImpl.kt
+++ app/src/main/java/com/hitanshudhawan/popcorn2/GenresRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.hitanshudhawan.popcorn2
 
 import androidx.lifecycle.liveData
 import com.hitanshudhawan.popcorn2.database.GenresDao
+import com.hitanshudhawan.popcorn2.database.MovieGenreEntity
+import com.hitanshudhawan.popcorn2.database.TVShowGenreEntity
 import com.hitanshudhawan.popcorn2.network.GenresService
 
 class GenresRepositoryImpl(
@@ -9,23 +11,37 @@ class GenresRepositoryImpl(
     private val genresDao: GenresDao
 ) : GenresRepository {
 
+    // hitanshu : make something like NetworkBoundResource
     override fun getMovieGenres() = liveData<Resource<List<Genre>>> {
         emit(Resource.Loading())
-        val response = genresService.getMovieGenres()
-        if (response.isSuccessful) {
-            emit(Resource.Success(response.body()!!.genres.map { Genre(it.id, it.name) }))
+        val genreEntities = genresDao.getMovieGenres()
+        if (genreEntities.isNotEmpty()) {
+            emit(Resource.Success(genreEntities.map { Genre(it.id, it.name) }))
         } else {
-            emit(Resource.Error())
+            val response = genresService.getMovieGenres()
+            if (response.isSuccessful) {
+                genresDao.insertMovieGenres(response.body()!!.genres.map { MovieGenreEntity(it.id, it.name) })
+                emit(Resource.Success(response.body()!!.genres.map { Genre(it.id, it.name) }))
+            } else {
+                emit(Resource.Error())
+            }
         }
     }
 
+    // hitanshu : make something like NetworkBoundResource
     override fun getTVShowGenres() = liveData<Resource<List<Genre>>> {
         emit(Resource.Loading())
-        val response = genresService.getTVShowGenres()
-        if (response.isSuccessful) {
-            emit(Resource.Success(response.body()!!.genres.map { Genre(it.id, it.name) }))
+        val genreEntities = genresDao.getTVShowGenres()
+        if (genreEntities.isNotEmpty()) {
+            emit(Resource.Success(genreEntities.map { Genre(it.id, it.name) }))
         } else {
-            emit(Resource.Error())
+            val response = genresService.getTVShowGenres()
+            if (response.isSuccessful) {
+                genresDao.insertTVShowGenres(response.body()!!.genres.map { TVShowGenreEntity(it.id, it.name) })
+                emit(Resource.Success(response.body()!!.genres.map { Genre(it.id, it.name) }))
+            } else {
+                emit(Resource.Error())
+            }
         }
     }
 
--- app/src/main/java/com/hitanshudhawan/popcorn2/database/GenresDao.kt
+++ app/src/main/java/com/hitanshudhawan/popcorn2/database/GenresDao.kt
@@ -1,6 +1,8 @@
 package com.hitanshudhawan.popcorn2.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -9,7 +11,13 @@ interface GenresDao {
     @Query("SELECT * FROM MovieGenreEntity")
     suspend fun getMovieGenres(): List<MovieGenreEntity>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovieGenres(genreEntities: List<MovieGenreEntity>)
+
     @Query("SELECT * FROM TVShowGenreEntity")
     suspend fun getTVShowGenres(): List<TVShowGenreEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTVShowGenres(genreEntities: List<TVShowGenreEntity>)
 
 }
