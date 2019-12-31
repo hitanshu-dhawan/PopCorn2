446b3c376b781a696341ec507ed1825ebc735faa
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-12-31 16:21:43 +0530

    First Cut: ViewAllMoviesUseCases and ViewAllMoviesRepository

--- app/src/main/java/com/hitanshudhawan/popcorn2/ViewAllMoviesPaginatedState.kt
+++ app/src/main/java/com/hitanshudhawan/popcorn2/ViewAllMoviesPaginatedState.kt
@@ -1,12 +1,12 @@
 package com.hitanshudhawan.popcorn2
 
 // hitanshu : make a base class UIState | or use Resource.kt
-sealed class ViewAllMoviesPaginatedState {
-
-    object Success : ViewAllMoviesPaginatedState()
-
-    object Error : ViewAllMoviesPaginatedState()
-
-    object Loading : ViewAllMoviesPaginatedState()
-
-}
+//sealed class ViewAllMoviesPaginatedState {
+//
+//    object Success : ViewAllMoviesPaginatedState()
+//
+//    object Error : ViewAllMoviesPaginatedState()
+//
+//    object Loading : ViewAllMoviesPaginatedState()
+//
+//}
---
+++ app/src/main/java/com/hitanshudhawan/popcorn2/ViewAllMoviesRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package com.hitanshudhawan.popcorn2
+
+class ViewAllMoviesRepositoryImpl : ViewAllMoviesRepository {
+
+    //...
+
+}
