9079babbdfaf498c4795f25858d8c3a3d7332ed6
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-06-23 18:27:10 +0530

    removed cache

--- app/src/main/java/com/hitanshudhawan/popcorn2/GenresRepositoryImpl.kt
+++ app/src/main/java/com/hitanshudhawan/popcorn2/GenresRepositoryImpl.kt
@@ -1,51 +1,25 @@
 package com.hitanshudhawan.popcorn2
 
-import com.hitanshudhawan.popcorn2.database.cache.CacheGenresDao
 import com.hitanshudhawan.popcorn2.network.GenresService
 
 class GenresRepositoryImpl(
-    private val genresService: GenresService,
-    private val cacheGenresDao: CacheGenresDao
+    private val genresService: GenresService
 ) : GenresRepository {
 
-    override suspend fun getMovieGenres() = resource<List<Genre>>(
-        network = {
-            val response = safe { genresService.getMovieGenres() }
-            if (response != null && response.isSuccessful)
-                Resource.Success(response.body()!!.mapToGenres())
-            else
-                Resource.Error()
-        },
-        database = {
-            val genreEntities = safe { cacheGenresDao.getMovieGenres() }
-            if (genreEntities != null && genreEntities.isNotEmpty())
-                Resource.Success(genreEntities.mapToGenres())
-            else
-                Resource.Error()
-        },
-        save = {
-            cacheGenresDao.insertMovieGenres(it.mapToMovieGenreEntities())
-        }
-    )
+    override suspend fun getMovieGenres(): Resource<List<Genre>> {
+        val response = safe { genresService.getMovieGenres() }
+        if (response != null && response.isSuccessful)
+            return Resource.Success(response.body()!!.mapToGenres())
+        else
+            return Resource.Error()
+    }
 
-    override suspend fun getTVShowGenres() = resource<List<Genre>>(
-        network = {
-            val response = safe { genresService.getTVShowGenres() }
-            if (response != null && response.isSuccessful)
-                Resource.Success(response.body()!!.mapToGenres())
-            else
-                Resource.Error()
-        },
-        database = {
-            val genreEntities = safe { cacheGenresDao.getTVShowGenres() }
-            if (genreEntities != null && genreEntities.isNotEmpty())
-                Resource.Success(genreEntities.mapToGenres())
-            else
-                Resource.Error()
-        },
-        save = {
-            cacheGenresDao.insertTVShowGenres(it.mapToTVShowGenreEntities())
-        }
-    )
+    override suspend fun getTVShowGenres(): Resource<List<Genre>> {
+        val response = safe { genresService.getTVShowGenres() }
+        if (response != null && response.isSuccessful)
+            return Resource.Success(response.body()!!.mapToGenres())
+        else
+            return Resource.Error()
+    }
 
 }
--- app/src/main/java/com/hitanshudhawan/popcorn2/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/hitanshudhawan/popcorn2/MoviesRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.hitanshudhawan.popcorn2
 
 import androidx.lifecycle.map
-import com.hitanshudhawan.popcorn2.database.cache.CacheMoviesDao
 import com.hitanshudhawan.popcorn2.database.favorite.FavoriteMovieBriefEntity
 import com.hitanshudhawan.popcorn2.database.favorite.FavoriteMoviesDao
 import com.hitanshudhawan.popcorn2.network.MoviesService
@@ -10,89 +9,40 @@ import kotlinx.coroutines.withContext
 
 class MoviesRepositoryImpl(
     private val moviesService: MoviesService,
-    private val cacheMoviesDao: CacheMoviesDao,
     private val favoriteMoviesDao: FavoriteMoviesDao
 ) : MoviesRepository {
 
-    override suspend fun getNowPlayingMovies(page: Int) = resource<List<MovieBrief>>(
-        network = {
-            val response = safe { moviesService.getNowPlayingMovies(page) }
-            if (response != null && response.isSuccessful)
-                Resource.Success(response.body()!!.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        database = {
-            val movieBriefEntities = safe { cacheMoviesDao.getNowPlayingMovies(page) }
-            if (movieBriefEntities != null && movieBriefEntities.isNotEmpty())
-                Resource.Success(movieBriefEntities.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        save = {
-            cacheMoviesDao.insertNowPlayingMovies(it.mapToNowPlayingMovieBriefEntities())
-        }
-    )
+    override suspend fun getNowPlayingMovies(page: Int): Resource<List<MovieBrief>> {
+        val response = safe { moviesService.getNowPlayingMovies(page) }
+        if (response != null && response.isSuccessful)
+            return Resource.Success(response.body()!!.mapToMovieBriefs())
+        else
+            return Resource.Error()
+    }
 
-    override suspend fun getPopularMovies(page: Int) = resource<List<MovieBrief>>(
-        network = {
-            val response = safe { moviesService.getPopularMovies(page) }
-            if (response != null && response.isSuccessful)
-                Resource.Success(response.body()!!.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        database = {
-            val movieBriefEntities = safe { cacheMoviesDao.getPopularMovies(page) }
-            if (movieBriefEntities != null && movieBriefEntities.isNotEmpty())
-                Resource.Success(movieBriefEntities.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        save = {
-            cacheMoviesDao.insertPopularMovies(it.mapToPopularMovieBriefEntities())
-        }
-    )
+    override suspend fun getPopularMovies(page: Int): Resource<List<MovieBrief>> {
+        val response = safe { moviesService.getPopularMovies(page) }
+        if (response != null && response.isSuccessful)
+            return Resource.Success(response.body()!!.mapToMovieBriefs())
+        else
+            return Resource.Error()
+    }
 
-    override suspend fun getUpcomingMovies(page: Int) = resource<List<MovieBrief>>(
-        network = {
-            val response = safe { moviesService.getUpcomingMovies(page) }
-            if (response != null && response.isSuccessful)
-                Resource.Success(response.body()!!.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        database = {
-            val movieBriefEntities = safe { cacheMoviesDao.getUpcomingMovies(page) }
-            if (movieBriefEntities != null && movieBriefEntities.isNotEmpty())
-                Resource.Success(movieBriefEntities.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        save = {
-            cacheMoviesDao.insertUpcomingMovies(it.mapToUpcomingMovieBriefEntities())
-        }
-    )
+    override suspend fun getUpcomingMovies(page: Int): Resource<List<MovieBrief>> {
+        val response = safe { moviesService.getUpcomingMovies(page) }
+        if (response != null && response.isSuccessful)
+            return Resource.Success(response.body()!!.mapToMovieBriefs())
+        else
+            return Resource.Error()
+    }
 
-    override suspend fun getTopRatedMovies(page: Int) = resource<List<MovieBrief>>(
-        network = {
-            val response = safe { moviesService.getTopRatedMovies(page) }
-            if (response != null && response.isSuccessful)
-                Resource.Success(response.body()!!.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        database = {
-            val movieBriefEntities = safe { cacheMoviesDao.getTopRatedMovies(page) }
-            if (movieBriefEntities != null && movieBriefEntities.isNotEmpty())
-                Resource.Success(movieBriefEntities.mapToMovieBriefs())
-            else
-                Resource.Error()
-        },
-        save = {
-            cacheMoviesDao.insertTopRatedMovies(it.mapToTopRatedMovieBriefEntities())
-        }
-    )
+    override suspend fun getTopRatedMovies(page: Int): Resource<List<MovieBrief>> {
+        val response = safe { moviesService.getTopRatedMovies(page) }
+        if (response != null && response.isSuccessful)
+            return Resource.Success(response.body()!!.mapToMovieBriefs())
+        else
+            return Resource.Error()
+    }
 
     override fun isFavoriteMovie(id: Int) = favoriteMoviesDao.getFavoriteMovieLiveData(id).map { it != null }
 
